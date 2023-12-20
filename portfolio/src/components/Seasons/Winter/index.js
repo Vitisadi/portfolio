880e6f8a817a07801db50ed8a971dceb1c518b27
Vitisadi <vitisadi@gmail.com>
2023-12-20 01:00:58 -0500

    Added winter

---
+++ portfolio/src/components/Seasons/Winter/index.js
@@ -0,0 +1,12 @@
+import "./index.css"
+
+const Winter = () => {
+    return (
+        <div className="winter-container">
+            Winter
+        </div>
+    )
+
+}
+
+export default Winter;
--- portfolio/src/components/Seasons/index.js
+++ portfolio/src/components/Seasons/index.js
@@ -1,10 +1,12 @@
 import "./index.css"
 import Spring from "./Spring"
+import Winter from "./Winter"
 
 const Seasons = () => {
     return (
         <div className="seasonsContainer">
             <Spring />
+            <Winter />
         </div>
     )
 }
