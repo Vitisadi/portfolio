0a6e4e2218c2188a2eaadf35f8a04fffd7e0d0fc
Vitisadi <vitisadi@gmail.com>
2023-12-21 19:29:17 -0500

    Made it larger

--- portfolio/src/components/Follow/index.js
+++ portfolio/src/components/Follow/index.js
@@ -4,32 +4,28 @@ import "./index.css";
 
 const Follow = () => {
     const [position, setPosition] = useState({ x: 0, y: 0 });
-    const [animate, setAnimate] = useState(false); // State to control animation
     const containerRef = useRef(null);
+    const imageWidth = 100; // Width of the image
+    const imageHeight = 150; // Height of the image
 
     const handleMouseMove = (e) => {
         if (containerRef.current) {
-            setAnimate(false); // Disable animation while moving
             const { left, top, width, height } = containerRef.current.getBoundingClientRect();
-            let newX = e.clientX - left - 25;
-            let newY = e.clientY - top - 25;
 
-            newX = Math.max(0, Math.min(newX, width - 50));
-            newY = Math.max(0, Math.min(newY, height - 50));
+            // Center the image on the cursor
+            let newX = e.clientX - left - (imageWidth / 2);
+            let newY = e.clientY - top - (imageHeight / 2);
+
+            // Confining the image within the container
+            newX = Math.max(0, Math.min(newX, width - imageWidth));
+            newY = Math.max(0, Math.min(newY, height - imageHeight));
 
             setPosition({ x: newX, y: newY });
         }
     };
 
     const handleMouseLeave = () => {
-        setAnimate(true); // Enable animation when mouse leaves
-        setPosition({ x: 0, y: 0 });
-    };
-
-    const imageStyle = {
-        left: `${position.x}px`,
-        top: `${position.y}px`,
-        transition: animate ? 'left 0.5s ease, top 0.5s ease' : 'none'
+        setPosition({ x: 0, y: 0 }); // Reset position
     };
 
     return (
@@ -39,7 +35,7 @@ const Follow = () => {
             onMouseLeave={handleMouseLeave} 
             className="follow-container"
         >
-            <img src={roseImg} alt="Rose" className="follow-image" style={imageStyle} />
+            <img src={roseImg} alt="Rose" className="follow-image" style={{ left: `${position.x}px`, top: `${position.y}px` }} />
         </div>
     );
 };
