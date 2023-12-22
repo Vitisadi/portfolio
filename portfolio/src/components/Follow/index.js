7298278c23cf822e81c44155751c237d4f91608d
Vitisadi <vitisadi@gmail.com>
2023-12-21 20:07:29 -0500

    Created the rose movement

--- portfolio/src/components/Follow/index.js
+++ portfolio/src/components/Follow/index.js
@@ -1,41 +1,91 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import roseImg from '../../assets/img/rose.png';
 import "./index.css";
 
 const Follow = () => {
-    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [position, setPosition] = useState({ x: 0, y: 0, rotate: 0, animate: false });
     const containerRef = useRef(null);
-    const imageWidth = 100; // Width of the image
-    const imageHeight = 150; // Height of the image
+    const firstMoveRef = useRef(true); // To track the first mouse move
 
-    const handleMouseMove = (e) => {
-        if (containerRef.current) {
-            const { left, top, width, height } = containerRef.current.getBoundingClientRect();
+    useEffect(() => {
+        const handleMouseMove = (e) => {
+            if (containerRef.current) {
+                const animate = firstMoveRef.current;
+                firstMoveRef.current = false; // After the first move, set to false
 
-            // Center the image on the cursor
-            let newX = e.clientX - left - (imageWidth / 2);
-            let newY = e.clientY - top - (imageHeight / 2);
+                const mouse = updateMouse(e.clientX, e.clientY);
+                const styles = getRoseStyles(mouse, animate);
+                setPosition(styles);
+            }
+        };
 
-            // Confining the image within the container
-            newX = Math.max(0, Math.min(newX, width - imageWidth));
-            newY = Math.max(0, Math.min(newY, height - imageHeight));
+        const handleMouseLeave = () => {
+            // Animate movement back to the start position with a slight rotation
+            setPosition({ x: 0, y: 0, rotate: -15, animate: true });
+        };
 
-            setPosition({ x: newX, y: newY });
-        }
+        const container = containerRef.current;
+        container.addEventListener('mousemove', handleMouseMove);
+        container.addEventListener('mouseleave', handleMouseLeave);
+
+        return () => {
+            container.removeEventListener('mousemove', handleMouseMove);
+            container.removeEventListener('mouseleave', handleMouseLeave);
+        };
+    }, []);
+
+    const updateMouse = (mouseX, mouseY) => {
+        const windowWidth = window.innerWidth;
+        const windowHeight = window.innerHeight;
+
+        return {
+            x: mouseX,
+            y: mouseY,
+            decimalX: mouseX / windowWidth,
+            decimalY: mouseY / windowHeight
+        };
     };
 
-    const handleMouseLeave = () => {
-        setPosition({ x: 0, y: 0 }); // Reset position
+    const getRoseStyles = (mouse, animate) => {
+        const container = containerRef.current.getBoundingClientRect();
+        const imageWidth = 100;  // Adjust to your image's width
+        const imageHeight = 150; // Adjust to your image's height
+
+        let x = mouse.x - (imageWidth / 2);
+        let y = mouse.y - (imageHeight / 2);
+
+        const offsetMultiplier = 80;
+        x += (mouse.decimalX - 0.5) * offsetMultiplier;
+
+        const rotation = (mouse.decimalX * 40 - 20); // Rotation degree
+
+        // Confine the image within the container
+        x = Math.max(0, Math.min(x, container.width - imageWidth));
+        y = Math.max(0, Math.min(y, container.height - imageHeight));
+
+        return {
+            x: x,
+            y: y,
+            rotate: rotation,
+            animate: animate
+        };
+    };
+
+    const imageStyle = {
+        left: `${position.x}px`, 
+        top: `${position.y}px`, 
+        transform: `rotate(${position.rotate}deg)`,
+        transition: position.animate ? 'left 0.5s ease, top 0.5s ease, transform 0.5s ease' : 'none'
     };
 
     return (
-        <div 
-            ref={containerRef} 
-            onMouseMove={handleMouseMove} 
-            onMouseLeave={handleMouseLeave} 
-            className="follow-container"
-        >
-            <img src={roseImg} alt="Rose" className="follow-image" style={{ left: `${position.x}px`, top: `${position.y}px` }} />
+        <div ref={containerRef} className="follow-container">
+            <img 
+                src={roseImg} 
+                alt="Rose" 
+                className="follow-image" 
+                style={imageStyle}
+            />
         </div>
     );
 };
