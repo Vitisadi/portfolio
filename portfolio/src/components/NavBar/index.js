4037cee6e9358bf6b83e372c24c7f8ac6dca0c9f
Vitisadi <vitisadi@gmail.com>
2023-12-22 18:29:39 -0500

    switched to about header

--- portfolio/src/components/NavBar/index.js
+++ portfolio/src/components/NavBar/index.js
@@ -44,7 +44,7 @@ const NavBar = () => {
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
             <Nav className="ms-auto">
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>About</Nav.Link>
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
             </Nav>
             <span className="navbar-text">
