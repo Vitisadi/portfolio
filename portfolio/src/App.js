fb462b2fcebcd05d7cd3ab4bf06814f89c7bb8da
Vitisadi <vitisadi@gmail.com>
2024-01-03 18:27:16 -0500

    WOP contact section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,8 +4,7 @@ import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-
-// import { Contact } from "./components/Contact";
+import Contact from "./components/Contact";
 // import { Footer } from "./components/Footer";
 
 function App() {
@@ -15,8 +14,8 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
-      {/* <Contact />
-      <Footer /> */}
+      <Contact />
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ portfolio/src/components/Contact/index.js
@@ -0,0 +1,80 @@
+import "./index.css"
+import { useState, useRef } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+// import contactImg from "../../assets/img/contact-img.svg";
+import 'animate.css';
+import emailjs from '@emailjs/browser'
+// import ProjectsSlider from "../ProjectsSlider";
+
+
+
+const Contact = () => {
+    const [buttonText, setButtonText] = useState('Send');
+    const refForm = useRef();
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        setButtonText("Sending...")
+
+        emailjs
+            .sendForm(
+                process.env.REACT_APP_EMAILJS_SERVICE_ID,
+                process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+                refForm.current,
+                process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+            )
+            .then(
+                () => {
+                    setButtonText("Send");
+                    alert('Message successfully sent!');
+                    window.location.reload(false);
+                },
+                () => {
+                    setButtonText("Send");
+                    alert('Failed to send the message, please try again.')
+                }
+            )
+    };
+
+    return (
+        <section className="contact" id="connect">
+            <Container>
+                <Row className="align-items-center">
+                    <Col size={12} md={6}>
+                        {/* <img src={contactImg} alt="Contact Us"/> */}
+                        {/* <ProjectsSlider></ProjectsSlider> */}
+                    </Col>
+                    <Col size={12} md={6}>
+                        <div>
+                            <h2>Get In Touch</h2>
+                            <form ref={refForm} onSubmit={handleSubmit}>
+                                <Row>
+                                    <Col size={12} sm={6} className="px-1">
+                                        <input type="text" name="fname" placeholder="First Name" required />
+                                    </Col>
+                                    <Col size={12} sm={6} className="px-1">
+                                        <input type="text" name="lname" placeholder="Last Name" required />
+                                    </Col>
+                                    <Col size={12} sm={6} className="px-1">
+                                        <input type="email" name="email" placeholder="Email Address" required />
+                                    </Col>
+                                    <Col size={12} sm={6} className="px-1">
+                                        <input type="tel" name="tel" placeholder="Phone Number" />
+                                    </Col>
+                                    <Col size={12} className="px-1">
+                                        <textarea rows="6" name="message" placeholder="Message" required ></textarea>
+                                        <button type="submit"><span>{buttonText}</span></button>
+                                    </Col>
+                                </Row>
+                            </form>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
+
+
+export default Contact
